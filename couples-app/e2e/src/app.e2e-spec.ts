942412b5cebdcb053fdeb3e3d8f5cfc58b5d42e8
Anthony Alaimo <haikyuuguy@Anthonys-MacBook-Pro-2.local>
2021-03-08 13:49:42 -0500

    fixing deployment issues

--- couples-app/e2e/src/app.e2e-spec.ts
+++ couples-app/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('dating-app app is running!');
+    expect(page.getTitleText()).toEqual('couples-app app is running!');
   });
 
   afterEach(async () => {
