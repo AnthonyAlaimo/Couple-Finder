116d2143c0c3e1826f095afe72f163a7b20a13e1
Anthony Alaimo <haikyuuguy@Anthonys-MacBook-Pro-2.local>
2021-03-09 10:56:06 -0500

    .

--- couples-app/server.js
+++ couples-app/server.js
@@ -1,9 +1,13 @@
 /*jshint esversion: 6 */
+const http = require("http");
 const express = require("express");
 const path = require("path");
 const app = express();
-app.use(express.static(__dirname + "/dist/couples-app"));
-app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/dist/couples-app/index.html"));
-});
-app.listen(process.env.PORT || 8080);
+app.use(express.static(path.join(__dirname, "path")));
+app.get("/*", (req, res) => res.sendFile(path.join(__dirname)));
+// Create the server on the specified port
+const server = http.createServer(app);
+const port = process.env.PORT || 3000;
+server.listen(port, () =>
+  console.log(`Server running on: http://localhost:${port}`)
+);
