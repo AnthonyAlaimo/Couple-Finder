67c459a51d1a3572ce9af70ed802494d94090de6
lialber2 <albertosaurus.li@mail.utoronto.ca>
2021-03-13 13:48:46 -0500

    Connected database to GraphQL api

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const upload = multer({dest: path.join(__dirname, "uploads")});
 const http = require('http');
 const fs = require('fs');
 const crypto = require('crypto');
+const axios = require('axios');
 
 const session = require("express-session");
 app.use(session({secret: crypto.randomBytes(16).toString("base64"), resave: false, saveUninitalized: true}));
@@ -27,25 +28,22 @@ http.createServer(app).listen(PORT, function (err) {
     else console.log("HTTP server on http://localhost:%s", PORT);
 });
 
-var images = new datastore({filename: 'db/images.db', autoload: true, timestampData: true});
-var comments = new datastore({filename: 'db/comments.db', autoload: true, timestampData: true});
-var users = new datastore({filename: 'db/users.db', autoload: true});
+/* HTTPS Proxy Server */
+app.all("*", function(req, res, next) {
+    if (req.headers['x-forwarded-proto'] != 'https') {
+        res.redirect("https://" + req.headers.host + req.url);
+    }
+    else {
+        next();
+    }
+});
+
+const databaseUrl = process.env.DATABASE_URL;
+const adminSecret = process.env.DATABASE_KEY;
 
 /* Object Constructors */
-function Image(author, title, imageData) {
-    this.author = author;
-    this.title = title;
-    this.image = imageData;
-}
-
-function Comment(author, content, imageId) {
-    this.author = author;
-    this.content = content;
-    this.imageId = imageId;
-}
-
-function User(username, password, salt) {
-    this._id = username;
+function User(email, password, salt) {
+    this.email = email;
     this.password = password;
     this.salt = salt;
 }
@@ -62,36 +60,15 @@ app.use(function (req, res, next) {
  * Sign up new user
  */
 app.post("/signup/", function (req, res, next) {
-    var username = req.body.username;
-    var password = req.body.password;
+    let username = req.body.username;
+    let password = req.body.password;
     // Determine if user already exists
-    users.findOne({_id: username}, function(err, user) {
-        if (err) {
-            return res.status(500).end(err);
-        }
-        else if (user) {
-            return res.status(409).end("username: " + username + " already exists");
-        }
-        else {
-            // Salt and hash password
-            let salt = crypto.randomBytes(16).toString("base64");
-            let hash = crypto.createHmac("sha512", salt);
-            hash.update(password);
-            users.update({_id: username}, new User(username, hash.digest("base64"), salt), {upsert: true}, function(err) {
-                if (err) {
-                    return res.status(500).end(err);
-                }
-                else {
-                    // Update session and cookies
-                    req.session.username = username;
-                    res.setHeader('Set-Cookie', cookie.serialize('username', username, {
-                        path : '/', 
-                        maxAge: 60 * 60 * 24 * 7
-                    }));
-                    return res.json("User: " + username + " signed up");
-                }
-            });
-        }
+    let salt = crypto.randomBytes(16).toString("base64");
+    let hash = crypto.createHmac("sha512", salt);
+    hash.update(password);
+
+    send("POST", "signup", new User(email, hash.digest("base64"), salt), function(response) {
+        console.log(response);    
     });
 });
 
@@ -426,4 +403,20 @@ function isAuthenticated(req, res, next) {
         return res.status(401).end("Access Denied");
     }
     next();
+}
+
+function send(method, action, data, callback){
+    let req = {user: data};
+    let config = {
+        headers: {
+            "content-type": "application/json",
+            "x-hasura-admin-secret": adminSecret
+        }
+    };
+    if (method === "POST") {
+        axios.post(databaseUrl + action, req, config).then((response) => callback(response))
+        .catch(function (error) {
+            console.log(error);
+        });
+    }
 }
