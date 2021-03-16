c75fbb7cf1a64362587df507cfb81fecfe73d9b5
Anthony Alaimo <haikyuuguy@Anthonys-MacBook-Pro-2.local>
2021-03-16 15:41:54 -0400

    updated profile setup

--- static/js/matches.js
+++ static/js/matches.js
@@ -7,23 +7,6 @@
     // Setting up profile
     document.querySelector("#filters").addEventListener("click", function (e) {
       api.toggle_visibility("#filter_options_display");
-<<<<<<< HEAD
-=======
-
-      // e.preventDefault();
-      // let name = document.querySelector("#profile_name").value;
-      // let age = document.querySelector("#profile_age").value;
-      // let gender = document.querySelector("#profile_gender").value;
-      // let bio = document.querySelector("#profile_bio").value;
-      // let picture = document.querySelector(
-      //   '#profile_submit input[name="image_file"]'
-      // ).files[0];
-      // document.querySelector("#profile_submit").reset();
-      // api.updateProfile(name, age, gender, bio, picture);
-      // document.getElementById("profile_setup").style.display = "none";
-      // document.getElementById("profile_design").style.display = "flex";
-      // document.getElementById("profile_survey").style.display = "flex";
->>>>>>> c9d9d6c1ee363cb62128cd71f30cf02780b1ec92
     });
   });
 })();
