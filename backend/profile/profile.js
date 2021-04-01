6204aa8446d11a15a8ddc0a8167241feb678f868
Anthony Alaimo <haikyuuguy@Anthonys-MacBook-Pro-2.local>
2021-04-01 13:34:15 -0400

    profile image code

--- backend/profile/profile.js
+++ backend/profile/profile.js
@@ -49,7 +49,7 @@ function updateUserProfile(req, res, next) {
         // Else allow modification of all fields
         else {
             // Check for missing fields
-            if (/*!req.file || */!req.body.name || !req.body.birth_date || !req.body.gender) {
+            if (!req.file || !req.body.name || !req.body.birth_date || !req.body.gender) {
                 return res.status(400)
                 .end("A required field is missing, please fix request and try again.");
             }
@@ -60,14 +60,14 @@ function updateUserProfile(req, res, next) {
                 age: calculateAge(req.body.birth_date),
                 gender: req.body.gender.toLocaleUpperCase("en-US"),
                 bio: req.body.bio,
-                /*pictures: {data: [
+                pictures: {data: [
                     {
                         path: req.file.path,
                         mimetype: req.file.mimetype,
                         filename: req.file.filename,
                         is_profile_picture: true
                     }
-                ]}*/
+                ]}
             }};
             database.put("profile/", data, function(resp, isError) {
                 if (resp.isAxiosError) {
--- frontend/src/utils/fetchApi.js
+++ frontend/src/utils/fetchApi.js
@@ -26,29 +26,3 @@
         ),
     ).then(data => data.json());
 }
-//for image
-// export function fetchImageApi(path, method = `GET`, body, signal) {
-//     let formdata = new FormData();
-//     Object.keys(data).forEach(function (key) {
-//       let value = data[key];
-//       formdata.append(key, value);
-//     });
-//     return fetch(
-//         `${process.env.REACT_APP_API_ENDPOINT}${path}`,
-//         Object.assign(
-//             {
-//                 headers: {
-//                     'Content-Type': 'multipart/form-data',
-//                     'Accept': 'application/json'
-//                 },
-//                 method,
-//             },
-//             method !== `GET` && {
-//                 body: formdata,
-//             },
-//             signal && {
-//                 signal,
-//             },
-//         ),
-//     ).then(formdata);
-// }
